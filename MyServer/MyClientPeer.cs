5dd839796e3d84b0fc7a8119df0e6571175e26ff
tianqi <1354636584@qq.com>
2020-04-16 15:01:18 +0800

    first

---
+++ MyServer/MyClientPeer.cs
@@ -0,0 +1,55 @@
+﻿using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using PhotonMessage;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameServer
+{
+    class MyClientPeer : ClientPeer
+    {
+        Hashtable userTabel;
+        public MyClientPeer(InitRequest initRequest) : base(initRequest)
+        {
+            userTabel = new Hashtable();
+            userTabel.Add("test1", "123");
+            userTabel.Add("test2", "123");
+            userTabel.Add("test3", "123");
+            userTabel.Add("test4", "123");
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            switch (operationRequest.OperationCode)
+            {
+                case (byte)OpCodeEnum.Login:
+                    string uname = (string)operationRequest.Parameters[(byte)OpKeyEnum.UserName];
+                    string pwd = (string)operationRequest.Parameters[(byte)OpKeyEnum.PassWord];
+
+                    if (userTabel.ContainsKey(uname) && userTabel[uname].Equals(pwd))
+                    {
+                        OperationResponse operation = new OperationResponse(operationRequest.OperationCode);
+                        operation.ReturnCode = (short)OpCodeEnum.LoginSuccess;
+                        operation.Parameters=new Dictionary<byte, object> { { (byte)OpKeyEnum.UserName, uname } };
+                        SendOperationResponse(operation, new SendParameters());
+                    }
+                    else
+                    {
+                        OperationResponse operation = new OperationResponse(operationRequest.OperationCode);
+                        operation.ReturnCode = (short)OpCodeEnum.LoginFailed;
+                        SendOperationResponse(operation, new SendParameters());
+                    }
+                    break;
+            }
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyServer/MyGameServer.cs
@@ -0,0 +1,49 @@
+﻿using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+using Photon.SocketServer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameServer
+{
+    public class MyGameServer : ApplicationBase  //继承接口，重写继承方法，注释不必要的异常抛出代码
+    {
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            log.Info("Client Request!");
+            MyClientPeer pongGamePeer = new MyClientPeer(initRequest);  //创建自己的Peer类对象并返回
+            return pongGamePeer;
+        }
+
+        protected override void Setup()
+        {
+            ////日志初始化
+            ////ApplicationRootPath为部署服务端程序目录 在这里指的是：E:\PhotonServerSDK\Photon-OnPremise-Server-SDK_v4-0-29-11263\deploy
+            //log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationRootPath, "log");//配置日志输出目录 同下
+            ////log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = @"E:\PhotonServerSDK\Photon-OnPremise-Server-SDK_v4-0-29-11263\deploy\log";//配置日志输出目录
+
+            ////BinaryPath为当前程序集的目录 在这里指的是：E:\PhotonServerSDK\Photon-OnPremise-Server-SDK_v4-0-29-11263\deploy\MyGameServer\bin
+            //FileInfo configFileInfo = new FileInfo(Path.Combine(this.BinaryPath, "log4net.config"));
+            //if (configFileInfo.Exists)
+            //{
+            //    //告知Photon使用的是log4net日志插件
+            //    LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+            //    //让log4net插件读取配置文件
+            //    XmlConfigurator.ConfigureAndWatch(configFileInfo);
+            //}
+
+            //log.Info("SetUp Completed!");
+        }
+
+        protected override void TearDown()
+        {
+            //log.Info("Server is shutdown.");
+        }
+    }
+}
